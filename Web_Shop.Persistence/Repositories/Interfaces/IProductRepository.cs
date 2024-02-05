119bc25957fee8979c7e251ad6e5326693d18d14
AdamRadziak <adam.radziak@gmail.com>
2024-02-05 20:26:54 +0100

    dodanie generycznego repo dla Produktów wraz z interfejsem

---
+++ Web_Shop.Persistence/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using WWSI_Shop.Persistence.MySQL.Model;
+
+namespace Web_Shop.Persistence.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task <bool> IsProductIdExistAsync(ulong ProductId);
+
+        Task<bool> IsProductNameExistAsync(string ProductName);
+        Task<Product?> GetProductByIdAsync(ulong ProductId);
+        Task<Product?> GetProductByNameAsync(string ProductName);
+    }
+}
---
+++ Web_Shop.Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_Shop.Persistence.Repositories;
+using Web_Shop.Persistence.Repositories.Interfaces;
+using WWSI_Shop.Persistence.MySQL.Context;
+using WWSI_Shop.Persistence.MySQL.Model;
+
+namespace Web_Shop.Persistence.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(WwsishopContext dbContext) : base(dbContext) { }
+
+        Task<Product?> IProductRepository.GetProductByIdAsync(ulong ProductId)
+        {
+            return Entities.FirstOrDefaultAsync(e => e.Equals(ProductId));
+        }
+
+        Task<Product?> IProductRepository.GetProductByNameAsync(string ProductName)
+        {
+            return Entities.FirstOrDefaultAsync(e => e.Equals(ProductName));
+        }
+
+        Task<bool> IProductRepository.IsProductIdExistAsync(ulong ProductId)
+        {
+            return Entities.AnyAsync(e => e.IdProduct == ProductId);
+        }
+
+        Task<bool> IProductRepository.IsProductNameExistAsync(string ProductName)
+        {
+            return Entities.AnyAsync(e => e.Name == ProductName);
+        }
+    }
+    }
+
